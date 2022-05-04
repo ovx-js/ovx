3158bc72ce1df62f4d198aab90f9909cf29c12a2
橙梓 <me@orangii.cn>
2022-05-04 22:59:17 +0800

    添加 Ant design 模板

--- src/template-list.ts
+++ src/template-list.ts
@@ -3,7 +3,8 @@ export interface TemplateList {
 }
 
 export const templates: TemplateList = {
-  'Vue-ts + Vue-Router + Element Plus with auto-import': 'vue-ts-element-plus',
+  'Vue-ts + Vue-Router + Element Plus  with auto-import': 'vue-ts-element-plus',
+  'Vue-ts + Vue-Router + Ant Design  with auto-import': 'vue-ts-ant-design',
 }
 
 export const templateList = Object.keys(templates)
