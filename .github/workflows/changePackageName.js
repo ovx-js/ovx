7529023bf46944f413b2eb3fa18ec1abefea0808
橙梓 <me@orangii.cn>
2022-05-23 09:20:55 +0800

    Update repo

--- .github/workflows/changePackageName.js
+++ .github/workflows/changePackageName.js
@@ -2,7 +2,7 @@ const fs = require("fs");
 
 let package = fs.readFileSync("./package.json", 'utf-8');
 let packageJson = JSON.parse(package);
-packageJson.name = "@Jiaocz/ovx";
+packageJson.name = "@ovx-js/ovx";
 package = JSON.stringify(packageJson);
-console.log(package);
+// console.log(package);
 fs.writeFileSync("./package.json", package, 'utf-8');
